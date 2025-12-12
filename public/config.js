9f21fa1f4f974e5fceb9d601c93fe43d098e15c9
moecinnamo <abcd2890000456@gmail.com>
2025-12-12 18:42:22 +0800

    增加配置项与图标库

---
+++ public/config.js
@@ -0,0 +1,13 @@
+window.$website = {
+    isDemo: false,
+    isDemoTip: '这是一个演示大屏',
+    title: 'DataBuddy 数据大屏',
+    name: 'DataBuddy 数据大屏',
+    subName: '你的数据可视化引擎',
+    url: 'https://databuddy.nyamiao.com/api/databuddy',
+    routers: {
+        mainPath: '/'
+    },
+    autoSave: false,
+    autoSaveTime: 60000,
+}
